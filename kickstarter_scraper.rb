651c92253b29508544bd4160a22b68b153133cab
Jonathan Hong <jonhong777@gmail.com>
2018-07-18 17:56:53 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ require 'pry'
 #   titles: project.css('h2.bbcard_name strong a').text
 #   images: project.css('div.project-thumbnail a img').attribute('src').value
 #   descriptions: project.css("p.bbcard_blurb").text
+#   location: project.css('ul.project-meta li a span.location-name').first.text
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
